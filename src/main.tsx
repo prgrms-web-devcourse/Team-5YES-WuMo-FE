6998b0d44a2df6132471598cf6c1cb91d42ae244
Yuri Kim <glassk0724@gmail.com>
2023-02-20 11:45:36 +0900

    chore: recoil, chakra-ui 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+import chakraTheme from '@chakra-ui/theme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
 import App from './App';
 
+const { Avatar, Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Avatar,
+    Button,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ChakraBaseProvider theme={theme}>
+        <App />
+      </ChakraBaseProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
