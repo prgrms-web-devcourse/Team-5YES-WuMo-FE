9e6937ad7e7c7193923f854187f52e4d245481db
JiYoung <yj0zero@gmail.com>
2023-02-21 20:39:16 +0900

    모임페이지 정보 섹션 구현  (#7)
    * feature: 모임페이지 router 추가
    
    * feature: 모임 페이지 정보 섹션 레이아웃 설정
    
    * chore: react icons 라이브러리 설치
    
    * feature: 모임 페이지 메뉴 탭 컴포넌트 구현
    
    * feature: 모임페이지 참여 유저 리스트 컴포넌트 구현
    
    * chore: husky, lint-staged 설정
    
    * style: eslint import 규칙 적용
    
    * refactor: 기본 button를 Chakra Button 컴포넌트로 변경
    
    ---------
    
    Co-authored-by: Yuri Kim <glassk0724@gmail.com>

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { MainPage, SignUpPage } from './pages';
+import PartyInformation from './components/Party/PartyInformation/PartyInformation';
+import {
+  MainPage,
+  PartyAlbumPage,
+  PartyNoticePage,
+  PartyPlanPage,
+  PartySchedulePage,
+  SignUpPage,
+} from './pages';
 import ROUTES from './utils/routes';
 
 const Router = () => {
@@ -9,6 +17,12 @@ const Router = () => {
       <Routes>
         <Route path={ROUTES.MAIN} element={<MainPage />} />
         <Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
+        <Route element={<PartyInformation />}>
+          <Route path={ROUTES.NOTICE} element={<PartyNoticePage />} />
+          <Route path={ROUTES.SCHEDULE} element={<PartySchedulePage />} />
+          <Route path={ROUTES.PLAN} element={<PartyPlanPage />} />
+          <Route path={ROUTES.ALBUM} element={<PartyAlbumPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Party/PartyInformation/PartyInformation.tsx
@@ -0,0 +1,34 @@
+import { Button, Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { BsFillShareFill } from 'react-icons/bs';
+import { Outlet } from 'react-router-dom';
+
+import PartyMenuTabList from './PartyMenuTabList';
+import PartyUserList from './PartyUserList';
+
+const PartyInformation = () => {
+  return (
+    <>
+      <Image src='https://via.placeholder.com/500x200' />
+      <Flex justify='space-between'>
+        <Container p='0.625rem' m='0'>
+          <Heading size='md'>파티 명</Heading>
+          <Text>기간</Text>
+        </Container>
+        <Flex p='10px' textAlign='right' align='center'>
+          <Button colorScheme='teal' size='xs' marginRight='0.625rem'>
+            영수증
+          </Button>
+          <Button bg='transparent' size='xs'>
+            <BsFillShareFill />
+          </Button>
+        </Flex>
+      </Flex>
+      <PartyUserList />
+      <Text margin='0.625rem'>파티 소개</Text>
+      <PartyMenuTabList />
+      <Outlet />
+    </>
+  );
+};
+
+export default PartyInformation;
