34038972ca34094eb676fbedd807df4f273febf5
JooChunWook <jha970107@gmail.com>
2023-02-21 17:49:39 +0900

    전역 스타일(reset-css, 폰트), 모바일 레이아웃 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,63 @@
+import { Center, Flex, Spacer } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { Common } from './styles/common';
+import globalStyle from './styles/globalStyle';
+
 function App() {
-  return <div className='App'>Wumo</div>;
+  return (
+    <GlobalLayout className='App'>
+      <Global styles={globalStyle} />
+      <div>
+        <h1>WuMo</h1>
+      </div>
+      <Navigation>
+        <Flex>
+          <Center>
+            <NavigationItem>홈</NavigationItem>
+          </Center>
+          <Spacer />
+          <Center>
+            <NavigationItem>관심</NavigationItem>
+          </Center>
+          <Spacer />
+          <Center>
+            <NavigationItem>파티 생성</NavigationItem>
+          </Center>
+          <Spacer />
+          <Center>
+            <NavigationItem>내 파티</NavigationItem>
+          </Center>
+          <Spacer />
+          <Center>
+            <NavigationItem>내 정보</NavigationItem>
+          </Center>
+        </Flex>
+      </Navigation>
+    </GlobalLayout>
+  );
 }
+
+export const GlobalLayout = styled.div`
+  max-width: ${Common.wideSize.mobile};
+  width: 100%;
+  height: 100%;
+  background-color: ${Common.colors.light_gray};
+  margin: 0 auto;
+`;
+
+export const Navigation = styled.nav`
+  position: fixed;
+  max-width: ${Common.wideSize.mobile};
+  width: 100%;
+  bottom: 0;
+  background-color: antiquewhite;
+  padding: 1rem;
+`;
+
+export const NavigationItem = styled.div`
+  font-size: ${Common.fontSize.md};
+`;
 
 export default App;
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+
+const globalStyle = css`
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  body {
+    height: 100%;
+  }
+
+  #root {
+    font-family: 'Pretendard-Regular';
+    height: 100%;
+  }
+  ${emotionReset}
+`;
+
+export default globalStyle;
