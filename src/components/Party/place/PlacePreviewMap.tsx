fdac880c5606044d80417931f231de6f5f46e950
Yuri Kim <glassk0724@gmail.com>
2023-02-23 03:42:47 +0900

    feat: 지도 마커 위치 미리보기

---
+++ src/components/Party/place/PlacePreviewMap.tsx
@@ -0,0 +1,36 @@
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+type Props = {
+  latitude: number;
+  longitude: number;
+  width?: string;
+  height?: string;
+  level?: number;
+};
+
+const PlacePreviewMap = ({
+  latitude,
+  longitude,
+  width = '100%',
+  height = '450px',
+  level = 3,
+}: Props) => {
+  return (
+    <Map
+      center={{
+        lat: latitude,
+        lng: longitude,
+      }}
+      isPanto={true}
+      draggable={false}
+      level={level}
+      style={{
+        width,
+        height,
+      }}>
+      <MapMarker position={{ lat: latitude, lng: longitude }} />
+    </Map>
+  );
+};
+
+export default PlacePreviewMap;
