e54db97b80960e4df940f8d95e392412379cb8c6
JiYoung <yj0zero@gmail.com>
2023-02-23 01:30:20 +0900

    메인페이지 레이아웃 구현 (#13)
    * chore: react-slick 설치
    
    * feature: 베스트 여행루트 섹션 캐러셀 구현
    
    * feature: 내 모임목록 섹션 캐러셀 구현
    
    * feature: 내 모임 공지 섹션 영역 잡기
    
    * feature: 메인 페이지에 컴포넌트 연결
    
    * chore: 베스트 여행 루트, 파티 리스트 router 주소 추가
    
    * feature: 더보기, 전체보기 버튼 router 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,19 @@ import { RecoilRoot } from 'recoil';
 
 import App from './App';
 
-const { Avatar, Button, Container, Input, FormLabel, Heading, Tabs } = chakraTheme.components;
+const { Avatar, Button, Container, Divider, Input, FormLabel, Heading, Tabs } = chakraTheme.components;
 
 const theme = extendBaseTheme({
   components: {
     Avatar,
     Button,
     Container,
+    Divider,
     Input,
     FormLabel,
     Heading,
     Tabs,
+    Divider,
   },
 });
 
