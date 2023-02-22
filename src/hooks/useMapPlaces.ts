c9c1060dc0b1007c5e46fd76e5e3686a41b4686e
Yuri Kim <glassk0724@gmail.com>
2023-02-23 02:00:17 +0900

    refactor: 지도 스크립트 추가와 장소 검색 로직 분리

--- src/hooks/useMapPlaces.ts
+++ src/hooks/useMapPlaces.ts
@@ -1,22 +1,19 @@
 import { useEffect, useState } from 'react';
 
+import useMapScript from './useMapScript';
+
 const useMapPlaces = () => {
   const [places, setPlaces] = useState<kakao.maps.services.Places>();
   const [result, setResult] = useState<kakao.maps.services.PlacesSearchResult>();
+  const { script } = useMapScript();
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
-      import.meta.env.VITE_KAKAO_API_JS_KEY
-    }&libraries=services&autoload=false`;
-
-    document.head.appendChild(script);
+    if (!script) return;
 
     script.addEventListener('load', () => {
       kakao.maps.load(() => setPlaces(new kakao.maps.services.Places()));
     });
-  }, []);
+  }, [script]);
 
   const searchPlaces = (keyword: string) => {
     if (!places) return;
