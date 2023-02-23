7bbda9a5b9652b9dac45502027cf1051025e9e4e
JiYoung <yj0zero@gmail.com>
2023-02-24 01:50:59 +0900

    메인 헤더 구현 (#25)
    * chore: 로고 svg 파일 추가
    
    * feature: 메인페이지 로고 헤더 추가
    
    * chore: 로그인 router 연결
    
    * chore: 로그인 router 주소 수정
    
    * refactor: sign in으로 명칭 통일

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import { Divider } from '@chakra-ui/react';
 
 import BestLouteList from '../components/main/BestLouteList';
+import MainHeader from '../components/main/MainHeader';
 import TotalPartyNoticeList from '../components/main/TotalPartyNoticeList';
 import UserPartyList from '../components/main/UserPartyList';
 
 const MainPage = () => {
   return (
     <>
+      <MainHeader />
       <BestLouteList />
       <Divider marginTop='40px' borderTopWidth='10px' />
       <UserPartyList />
