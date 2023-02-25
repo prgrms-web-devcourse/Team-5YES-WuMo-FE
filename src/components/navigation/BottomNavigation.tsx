3a503158f7eca25c7b16df8f727769d209417aa2
mxx-kor <minjae7728@gmail.com>
2023-02-25 18:34:01 +0900

    refactor: signin, signup 하단 navbar 정리

--- src/components/navigation/BottomNavigation.tsx
+++ src/components/navigation/BottomNavigation.tsx
@@ -1,30 +1,33 @@
 import { Box, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 import { NAVIGATION_ITEM } from '@/src/utils/constants/navigationItem';
 
 const BottomNavigation = () => {
   const { pathname } = useLocation();
   return (
-    <Navigation maxW='maxWidth.mobile' bg='white'>
-      <Flex justify='space-between'>
-        {NAVIGATION_ITEM.map((item) => (
-          <Link to={item.link} key={item.id}>
-            <GridItem display='flex' alignItems='center' flexDirection='column' gap='2'>
-              <Box>
-                {pathname === item.link ? (
-                  <item.activeIcon style={{ width: '28px', height: '28px' }} />
-                ) : (
-                  <item.icon style={{ width: '28px', height: '28px' }} />
-                )}
-              </Box>
-              <Text fontSize='sm'>{item.name}</Text>
-            </GridItem>
-          </Link>
-        ))}
-      </Flex>
-    </Navigation>
+    <>
+      <Navigation maxW='maxWidth.mobile'>
+        <Flex justify='space-between'>
+          {NAVIGATION_ITEM.map((item) => (
+            <Link to={item.link} key={item.id}>
+              <GridItem display='flex' alignItems='center' flexDirection='column' gap='2'>
+                <Box>
+                  {pathname === item.link ? (
+                    <item.activeIcon style={{ width: '28px', height: '28px' }} />
+                  ) : (
+                    <item.icon style={{ width: '28px', height: '28px' }} />
+                  )}
+                </Box>
+                <Text fontSize='sm'>{item.name}</Text>
+              </GridItem>
+            </Link>
+          ))}
+        </Flex>
+      </Navigation>
+      <Outlet />
+    </>
   );
 };
 
