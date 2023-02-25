6c3fc6bbb3fbde40e1fcdda0a70217b047676c2f
JiYoung <yj0zero@gmail.com>
2023-02-25 16:13:57 +0900

    buttom sheet 모달 구현 (#31)
    * feature: bottomSheet Modal 구현
    
    * feature: 공통 Textarea 컴포넌트 구현
    
    * chore: body 스크롤 hidden 설정
    
    * refactor: bottomSheet type 분리
    
    * rename: bottomSheet type 파일 명 오타 수정
    
    * refactor: buttonText ➡️ buttonTitle로 수정

---
+++ src/components/base/BottomSheet.tsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Center,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+import { BottomSheetProps } from '@/src/types/bottomSheet';
+
+const BottomSheet = ({ isOpen, onClose, modal }: BottomSheetProps) => {
+  return (
+    <>
+      <Modal isCentered onClose={onClose} isOpen={isOpen} motionPreset='slideInBottom'>
+        <ModalOverlay height='100vh' maxW='35rem' left='0' right='0' margin='0 auto' />
+        <ModalContent
+          as={motion.div}
+          position='fixed'
+          bottom='0'
+          mb='0'
+          borderRadius='1.75rem 1.75rem 0 0'
+          maxW='35rem'
+          initial={{ y: 100 }}
+          animate={{
+            y: 0,
+            transition: {
+              type: 'spring',
+              bounce: 0,
+              duration: 0.3,
+            },
+          }}>
+          <ModalHeader fontSize='2xl'>{modal.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Center flexDir='column' mt='6' mb='4'>
+              {modal.content}
+              <Button
+                bg='primary.red'
+                color='white'
+                size='lg'
+                w='99%'
+                marginTop='40px'
+                onClick={() => {
+                  onClose();
+                  modal.onClick && modal.onClick();
+                }}
+                borderRadius='2xl'>
+                {modal.buttonText}
+              </Button>
+            </Center>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default BottomSheet;
