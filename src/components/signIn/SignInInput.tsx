60617deec0644d54956f99b157eb092f91dd9405
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2023-02-25 17:27:01 +0900

    로그인 페이지 구현 (#29)
    * rename: userSign으로 이름 변경
    
    * feautre: signin 기능 구현
    
    * feature: localstorage hook 추가
    
    * refactor: 오타 수정
    
    * refactor: 파일명 변경

---
+++ src/components/signIn/SignInInput.tsx
@@ -0,0 +1,55 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { InputHTMLAttributes } from 'react';
+import {
+  Control,
+  FieldPath,
+  FieldValues,
+  useController,
+  UseFormResetField,
+} from 'react-hook-form';
+import { MdCancel } from 'react-icons/md';
+
+interface UserInputProps<T extends FieldValues>
+  extends InputHTMLAttributes<HTMLInputElement> {
+  control: Control<T>;
+  name: FieldPath<T>;
+  resetField: UseFormResetField<T>;
+}
+
+const SignInInput = <T extends FieldValues>({
+  name,
+  control,
+  resetField,
+}: UserInputProps<T>) => {
+  const { field, fieldState } = useController({ name, control });
+  const upperName = name.replace(/^[a-z]/, (char: string) => char.toUpperCase());
+
+  return (
+    <FormControl isInvalid={!!fieldState.error?.message}>
+      <FormLabel>{upperName}</FormLabel>
+      <InputGroup size='md'>
+        <Input
+          id={name}
+          placeholder={upperName}
+          type={name === 'password' ? 'password' : 'text'}
+          {...field}
+        />
+        <InputRightElement>
+          {fieldState.isDirty && (
+            <MdCancel cursor='pointer' onClick={() => resetField(name)} />
+          )}
+        </InputRightElement>
+      </InputGroup>
+      <FormErrorMessage>{fieldState.error?.message}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default SignInInput;
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+  const setValue = (value: string) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
