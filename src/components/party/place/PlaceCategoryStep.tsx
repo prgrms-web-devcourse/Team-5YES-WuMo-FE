02c101b837efce22fca50b0c275868789485d0bb
Yuri Kim <glassk0724@gmail.com>
2023-02-26 22:38:32 +0900

    feat: 장소 카테고리 선택 기능 추가

--- src/components/party/place/PlaceCategoryStep.tsx
+++ src/components/party/place/PlaceCategoryStep.tsx
@@ -1,5 +1,32 @@
+import { Flex, Grid, GridItem, Image } from '@chakra-ui/react';
+import { useState } from 'react';
+
+import { categoryList } from '@/src/utils/constants/place';
+
 const PlaceCategoryStep = () => {
-  return <div>카테고리 선택</div>;
+  const [value, setValue] = useState('');
+
+  return (
+    <Grid templateColumns='repeat(3, 1fr)' templateRows='repeat(3, 1fr)' gap='3'>
+      {categoryList.map(({ name, text, imageUrl }) => (
+        <GridItem
+          key={name}
+          cursor='pointer'
+          borderRadius='2xl'
+          paddingTop='3.5'
+          paddingBottom='3.5'
+          fontWeight={value === text ? 'bold' : 'initial'}
+          backgroundColor={value === text ? 'gray.100' : 'gray.50'}
+          _hover={{ backgroundColor: 'gray.100', fontWeight: 'bold' }}
+          onClick={() => setValue(text)}>
+          <Flex direction='column' align='center' justify='center' gap='4'>
+            <Image src={imageUrl} alt={text} width='2rem' />
+            {text}
+          </Flex>
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
 
 export default PlaceCategoryStep;
---
+++ src/utils/constants/place.ts
@@ -0,0 +1,27 @@
+export const description = [
+  {
+    subtitle: '가게명',
+    example: '스타벅스, 맥도날드',
+  },
+  {
+    subtitle: '장소명',
+    example: '강남역, 양재역',
+  },
+  {
+    subtitle: '주소지',
+    example: '강남대로 100길',
+  },
+];
+
+export const categoryList = [
+  {
+    name: 'rice',
+    text: '식사',
+    imageUrl: 'https://github.githubassets.com/images/icons/emoji/unicode/1f35a.png?v8',
+  },
+  {
+    name: 'coffee',
+    text: '커피',
+    imageUrl: 'https://github.githubassets.com/images/icons/emoji/unicode/2615.png?v8',
+  },
+];
