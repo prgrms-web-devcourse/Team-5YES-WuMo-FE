f8504d04dacc62e81fb6312451e5397badb54db3
mxx-kor <minjae7728@gmail.com>
2023-02-28 02:17:04 +0900

    hotfix: pull 뒤 에러 수정

--- src/pages/PlaceCreatePage.tsx
+++ src/pages/PlaceCreatePage.tsx
@@ -1,15 +1,11 @@
 import { Button, useDisclosure } from '@chakra-ui/react';
 
-import PlaceSearchModal from '@/components/party/place/PlaceSearchModal';
-
 const PlaceCreatePage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <Button onClick={onOpen}>장소 검색</Button>
-
-      <PlaceSearchModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
