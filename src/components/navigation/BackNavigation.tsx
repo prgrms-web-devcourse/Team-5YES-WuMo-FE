84b6d1ce1115ae81409bfd360f2d7b3456e02066
JooChunWook <jha970107@gmail.com>
2023-03-01 23:19:31 +0900

    feature: 베스트 루트 목록 검색 폼 구현

--- src/components/navigation/BackNavigation.tsx
+++ src/components/navigation/BackNavigation.tsx
@@ -1,27 +1,51 @@
-import { Container, Flex } from '@chakra-ui/react';
+import { Button, Container, Flex, Input } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { MdKeyboardArrowLeft } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 
 import { BackNavigationProps } from '@/types/backNavigation';
 
 const BackNavigation = ({ title, option }: BackNavigationProps) => {
+  const [isShowSearch, setIsShowSearch] = useState(false);
+
   const navigate = useNavigate();
   return (
     <Nav maxW='maxWidth.mobile' bg='white' zIndex='20'>
-      <Flex justify='space-between'>
+      <Flex justify='space-between' mb='4'>
         <SpanButton onClick={() => navigate(-1)}>
           <MdKeyboardArrowLeft />
         </SpanButton>
         <Title>{title}</Title>
-        <SpanButton>{option}</SpanButton>
+        <SpanButton onClick={() => setIsShowSearch(!isShowSearch)}>{option}</SpanButton>
       </Flex>
+      {option && isShowSearch ? (
+        <Flex
+          px='1rem'
+          justifyContent='space-between'
+          as={motion.div}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}>
+          <Input
+            placeholder='검색어를 입력하세요.'
+            w='80%'
+            fontSize='0.75rem'
+            padding='8px'
+          />
+          <Button fontSize='0.875rem'>검색</Button>
+        </Flex>
+      ) : (
+        ''
+      )}
     </Nav>
   );
 };
 
 const SpanButton = styled.span`
   cursor: pointer;
+  font-size: 1.5rem;
+  padding-top: 4px;
 `;
 
 const Nav = styled(Container)`
