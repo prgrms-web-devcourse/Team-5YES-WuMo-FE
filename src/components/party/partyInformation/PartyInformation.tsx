9180f946b154b7b807bcb3a8803b94106073c892
유지영 <yj0zero@gmail.com>
2023-03-01 15:55:03 +0900

    design: 파티 페이지 backNavigation 설정 및 공지 탭 제거

--- src/components/party/partyInformation/PartyInformation.tsx
+++ src/components/party/partyInformation/PartyInformation.tsx
@@ -1,14 +1,17 @@
-import { Button, Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import { BsFillShareFill } from 'react-icons/bs';
 import { Outlet } from 'react-router-dom';
+
+import BackNavigation from '@/components/navigation/BackNavigation';
 
 import PartyMenuTabList from './PartyMenuTabList';
 import PartyUserList from './PartyUserList';
 
 const PartyInformation = () => {
   return (
-    <>
-      <Image src='https://via.placeholder.com/500x200' />
+    <Box>
+      <BackNavigation />
+      <Image src='https://via.placeholder.com/560x200' pt='48px' />
       <Flex justify='space-between'>
         <Container p='0.625rem' m='0'>
           <Heading size='md'>파티 명</Heading>
@@ -24,10 +27,14 @@ const PartyInformation = () => {
         </Flex>
       </Flex>
       <PartyUserList />
-      <Text margin='0.625rem'>파티 소개</Text>
+      <Text margin='0.625rem' h='3.125rem'>
+        파티 소개
+      </Text>
       <PartyMenuTabList />
-      <Outlet />
-    </>
+      <Box>
+        <Outlet />
+      </Box>
+    </Box>
   );
 };
 
