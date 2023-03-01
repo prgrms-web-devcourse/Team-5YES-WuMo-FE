4eb62bf102eaa96394e59ab0dd87288f4176db29
유지영 <yj0zero@gmail.com>
2023-03-01 15:57:24 +0900

    feature: 최종 루트 타임라인 컴포넌트 구현

--- src/types/schedule.d.ts
+++ src/types/schedule.d.ts
@@ -18,3 +18,17 @@ export type CommentFeedTitleProps = {
 export type AmountType = {
   amount: string;
 };
+
+export type TimeLineProps = {
+  onClickhandler?: () => void;
+  routerButton?: JSX.Element;
+};
+
+export type routeListProps = {
+  name: string;
+  address: string;
+  image: string;
+  visitDate: string;
+  spending: number;
+  category: string;
+} & TimeLineProps;
