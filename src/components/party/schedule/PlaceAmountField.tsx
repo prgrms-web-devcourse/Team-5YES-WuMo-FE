c62cbdae93d3ecb85331baf6adc95cf54ceaa06d
유지영 <yj0zero@gmail.com>
2023-03-03 18:47:56 +0900

    refactor: 댓글 피드 페이지 아코디언 제거

--- src/components/party/schedule/PlaceAmountField.tsx
+++ src/components/party/schedule/PlaceAmountField.tsx
@@ -1,6 +1,6 @@
 import {
-  AccordionPanel,
   Button,
+  Container,
   Flex,
   Input,
   InputGroup,
@@ -31,37 +31,35 @@ const PlaceAmountField = () => {
   };
 
   return (
-    <div>
-      <AccordionPanel as='form' onSubmit={handleSubmit(onSubmitAmount)} p='0.5rem 0'>
-        <Flex align='center' mt='0.5rem'>
-          <Text pr='0.625rem'>사용금액</Text>
-          <InputGroup size='sm' maxW='12.5rem'>
-            <InputLeftElement
-              pointerEvents='none'
-              color='gray'
-              fontSize='1.5625rem'
-              ml='0.25rem'>
-              <MdCreditCard />
-            </InputLeftElement>
-            <Input
-              type='number'
-              borderRadius='0.9375rem'
-              placeholder='사용 금액을 입력하세요'
-              {...register('amount')}
-            />
-            <InputRightElement>원</InputRightElement>
-          </InputGroup>
-          <Button
-            type='submit'
-            variant='outline'
-            size='sm'
+    <Container as='form' onSubmit={handleSubmit(onSubmitAmount)} p='0.5rem 0'>
+      <Flex align='center' mt='0.5rem'>
+        <Text pr='0.625rem'>사용금액</Text>
+        <InputGroup size='sm' maxW='12.5rem'>
+          <InputLeftElement
+            pointerEvents='none'
+            color='gray'
+            fontSize='1.5625rem'
+            ml='0.25rem'>
+            <MdCreditCard />
+          </InputLeftElement>
+          <Input
+            type='number'
             borderRadius='0.9375rem'
-            marginLeft='auto'>
-            수정
-          </Button>
-        </Flex>
-      </AccordionPanel>
-    </div>
+            placeholder='사용 금액을 입력하세요'
+            {...register('amount')}
+          />
+          <InputRightElement>원</InputRightElement>
+        </InputGroup>
+        <Button
+          type='submit'
+          variant='outline'
+          size='sm'
+          borderRadius='0.9375rem'
+          marginLeft='auto'>
+          수정
+        </Button>
+      </Flex>
+    </Container>
   );
 };
 
