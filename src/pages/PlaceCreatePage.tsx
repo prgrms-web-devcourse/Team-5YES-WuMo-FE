da273e06f696a74885c41e4658be3bd9085181d0
Yuri Kim <glassk0724@gmail.com>
2023-03-05 04:26:08 +0900

    feat: 후보지 추가 모달 닫기 전 alert, 닫으면 데이터 초기화

--- src/pages/PlaceCreatePage.tsx
+++ src/pages/PlaceCreatePage.tsx
@@ -8,14 +8,22 @@ import {
 import { useEffect, useState } from 'react';
 import { MdKeyboardArrowLeft } from 'react-icons/md';
 import { useNavigate } from 'react-router';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 
-import { createPlaceStepState } from '@/store/recoilPlaceState';
+import {
+  createPlaceState,
+  createPlaceStepState,
+  initialPlaceSearchState,
+  initialPlaceState,
+  placeSearchState,
+} from '@/store/recoilPlaceState';
 import { placeCreateStepItems, processStep } from '@/utils/constants/processStep';
 import ROUTES from '@/utils/constants/routes';
 
 const PlaceCreatePage = () => {
   const navigate = useNavigate();
+  const setCreatePlaceBody = useSetRecoilState(createPlaceState);
+  const setPlaceSearchState = useSetRecoilState(placeSearchState);
   const [step, setStep] = useRecoilState<number>(createPlaceStepState);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -26,6 +34,11 @@ const PlaceCreatePage = () => {
   const handleCloseModal = () => {
     if (step > processStep.min) setStep(step - 1);
     else {
+      const canClose = confirm('후보지 추가를 취소하시겠습니까?'); // TODO: 값 선택한 경우에만 하도록 처리
+      if (!canClose) return;
+
+      setCreatePlaceBody(initialPlaceState);
+      setPlaceSearchState(initialPlaceSearchState);
       setIsOpen(false);
       navigate(ROUTES.PLAN);
     }
--- src/store/recoilPlaceState.ts
+++ src/store/recoilPlaceState.ts
@@ -2,19 +2,34 @@ import { atom } from 'recoil';
 
 import { Place, PlaceSearchResult } from '@/types/place';
 
+export const initialPlaceState = {
+  name: '',
+  address: '',
+  latitude: 0,
+  longitude: 0,
+  image: '',
+  category: '',
+  description: '',
+  visitDate: '',
+  expectedCost: 0,
+};
+
+export const initialPlaceSearchState = {
+  keyword: '',
+  result: [],
+  selectedPlace: {
+    id: '',
+    place_name: '',
+    address_name: '',
+    road_address_name: '',
+    x: '',
+    y: '',
+  },
+};
+
 export const createPlaceState = atom<Place>({
   key: 'createPlaceState',
-  default: {
-    name: '',
-    address: '',
-    latitude: 0,
-    longitude: 0,
-    image: '',
-    category: '',
-    description: '',
-    visitDate: '',
-    expectedCost: 0,
-  },
+  default: initialPlaceState,
 });
 
 export const createPlaceStepState = atom<number>({
@@ -24,16 +39,5 @@ export const createPlaceStepState = atom<number>({
 
 export const placeSearchState = atom<PlaceSearchResult>({
   key: 'placeSearchState',
-  default: {
-    keyword: '',
-    result: [],
-    selectedPlace: {
-      id: '',
-      place_name: '',
-      address_name: '',
-      road_address_name: '',
-      x: '',
-      y: '',
-    },
-  },
+  default: initialPlaceSearchState,
 });
