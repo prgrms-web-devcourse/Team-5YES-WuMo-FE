bf5e54e17de23f76af013a76779f4470860b6ceb
Yuri Kim <glassk0724@gmail.com>
2023-03-05 16:39:08 +0900

    확인 모달 레이아웃 구현 (#95)
    * feat: 확인 모달 컴포넌트 구현
    
    * feat: 확인 모달 컴포넌트 사용 예시
    
    * refactor: ConfirmModalProps 타입 분리
    
    * refactor: ConfirmModal left와 right 표현 변경
    - left -> secondary
    - right -> primary
    
    * design: 버튼 패딩 통일

--- src/pages/PartyPlanPage.tsx
+++ src/pages/PartyPlanPage.tsx
@@ -1,8 +1,7 @@
-import { useDisclosure } from '@chakra-ui/react';
+import { Box, Button, Text, useDisclosure } from '@chakra-ui/react';
 import { useEffect } from 'react';
 
-import PlaceList from '@/components/party/partyPlan/PlanPlaceList';
-import PlaceCreateModal from '@/components/placeCreate/PlaceCreateModal';
+import ConfirmModal from '@/components/base/ConfirmModal';
 import PlacePreviewMap from '@/components/placeCreate/search/PlacePreviewMap';
 import useMapScript from '@/hooks/useMapScript';
 import { PLACES_DUMMY_DATA } from '@/utils/constants/place';
@@ -27,8 +26,22 @@ const PartyPlanPage = () => {
         mapMarkers={PLACES_DUMMY_DATA}
         draggable
       />
-      <PlaceList places={PLACES_DUMMY_DATA} openModalHandler={onOpen} />
-      <PlaceCreateModal isOpen={isOpen} closeModalHandler={onClose} />
+      <Button onClick={onOpen}>모달 테스트</Button>
+      {/* <PlaceList places={PLACES_DUMMY_DATA} openModalHandler={onOpen} />
+      <PlaceCreateModal isOpen={isOpen} closeModalHandler={onClose} /> */}
+      <ConfirmModal
+        isOpen={isOpen}
+        closeModalHandler={onClose}
+        body={
+          <Box textAlign='center'>
+            <Text>장소: 정통집</Text>
+            <Text>날짜: 2023년 3월 5일 오후 6시</Text>
+            <Text>이 후보지를 일정을 추가할까요?</Text>
+          </Box>
+        }
+        buttonText={{ secondary: '수정', primary: '추가' }}
+        clickButtonHandler={{ primary: () => console.log('추가 완료') }}
+      />
     </>
   );
 };
