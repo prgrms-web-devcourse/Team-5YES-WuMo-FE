f95884fedbfb8a3c9ec635f6afeec592ba3e55bc
mxx-kor <minjae7728@gmail.com>
2023-03-06 17:21:20 +0900

    refactor: 닉네임 스키마, 에러메시지 추가

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -5,6 +5,7 @@ import { FORM_ERROR_MESSAGES } from './constants/messages';
 const regex_spacing = /^[^\s]*$/;
 const regex_inword = /^[^ㄱ-ㅎ|ㅏ-ㅣ]*$/;
 const regex_password = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/;
+const regex_nickname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
 const regex_nospecial = /^[a-zA-Z0-9@.]*$/;
 
 export const signUpSchema = yup.object({
@@ -19,7 +20,7 @@ export const signUpSchema = yup.object({
     .required(FORM_ERROR_MESSAGES.NICKNAME_REQUIRED)
     .matches(regex_inword, FORM_ERROR_MESSAGES.NICKNAME_INWORD)
     .matches(regex_spacing, FORM_ERROR_MESSAGES.SPACING)
-    .matches(regex_nospecial, FORM_ERROR_MESSAGES.SPEACIAL)
+    .matches(regex_nickname, FORM_ERROR_MESSAGES.NICKNAME_PATTERN)
     .max(20, FORM_ERROR_MESSAGES.MAX(20)),
   password: yup
     .string()
@@ -41,4 +42,22 @@ export const signInSchema = yup.object({
     .required(FORM_ERROR_MESSAGES.EMAIL_REQUIRED)
     .email(FORM_ERROR_MESSAGES.EMAIL_PATTERN),
   password: yup.string().required(FORM_ERROR_MESSAGES.PASSWORD_REQUIRED),
+});
+
+export const userEditSchema = yup.object({
+  nickname: yup
+    .string()
+    .required(FORM_ERROR_MESSAGES.NICKNAME_REQUIRED)
+    .matches(regex_inword, FORM_ERROR_MESSAGES.NICKNAME_INWORD)
+    .matches(regex_spacing, FORM_ERROR_MESSAGES.SPACING)
+    .matches(regex_nickname, FORM_ERROR_MESSAGES.NICKNAME_PATTERN)
+    .max(20, FORM_ERROR_MESSAGES.MAX(20)),
+  password: yup
+    .string()
+    .required(FORM_ERROR_MESSAGES.PASSWORD_REQUIRED)
+    .matches(regex_password, FORM_ERROR_MESSAGES.PASSWORD_PATTERN)
+    .matches(regex_spacing, FORM_ERROR_MESSAGES.SPACING)
+    .matches(regex_nospecial, FORM_ERROR_MESSAGES.SPEACIAL)
+    .min(8, FORM_ERROR_MESSAGES.MIN(8))
+    .max(12, FORM_ERROR_MESSAGES.MAX(12)),
 });
