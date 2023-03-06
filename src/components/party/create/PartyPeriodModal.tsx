38199077f9870a87c9e98f893cae3cc3a0ae28fb
JooChunWook <jha970107@gmail.com>
2023-03-07 01:14:24 +0900

    hotfix: react-calendar 타입에러 해결

--- src/components/party/create/PartyPeriodModal.tsx
+++ src/components/party/create/PartyPeriodModal.tsx
@@ -1,29 +1,28 @@
 import 'react-calendar/dist/Calendar.css';
 
-import { Box, Flex, ModalBody, ModalFooter, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, ModalBody, ModalFooter, Text } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import { useRecoilState } from 'recoil';
 
-import ModalButton from '@/components/base/ModalButton';
 import { createPartyState, stepState } from '@/store/recoilPartyState';
 import { PartyCreateBody } from '@/types/party';
 import { processStep } from '@/utils/constants/processStep';
 
 const PartyPeriodModal = () => {
-  const [value, onChange] = useState<any>(new Date());
-  console.log(value);
+  const [value, onChange] = useState([new Date(), new Date()]);
 
   const [createPartyBody, setCreatePartyBody] =
     useRecoilState<PartyCreateBody>(createPartyState);
   const [step, setStep] = useRecoilState<number>(stepState);
 
   const onClickNextStep = () => {
+    const [startDate, endDate] = value;
     setCreatePartyBody({
       ...createPartyBody,
-      startDate: dayjs(value[0]).format('YYYY-MM-DD'),
-      endDate: dayjs(value[1]).format('YYYY-MM-DD'),
+      startDate: dayjs(startDate).format('YYYY-MM-DD'),
+      endDate: dayjs(endDate).format('YYYY-MM-DD'),
     });
   };
 
@@ -33,8 +32,7 @@ const PartyPeriodModal = () => {
         <Flex flexDirection='column' justify='center' alignItems='center'>
           <Box mb='10' textAlign='center'>
             <Calendar
-              onChange={onChange}
-              // value={value}
+              onChange={(value: Date[]) => onChange(value)}
               formatDay={(_, date) => dayjs(date).format('DD')}
               allowPartialRange={true}
               selectRange={true}
@@ -63,15 +61,21 @@ const PartyPeriodModal = () => {
         </Flex>
       </ModalBody>
       <ModalFooter>
-        <ModalButton
-          text='다음'
-          clickButtonHandler={() => {
+        <Button
+          bg='primary.red'
+          color='#ffffff'
+          _hover={{
+            bg: 'primary.redHover',
+          }}
+          w='full'
+          onClick={() => {
             if (step !== processStep.partyCreateMax) {
               onClickNextStep();
               setStep(step + 1);
             }
-          }}
-        />
+          }}>
+          다음
+        </Button>
       </ModalFooter>
     </>
   );
