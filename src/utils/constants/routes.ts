1fd7c1942d1cc5b86a48e6057e992fcdf5f5345f
JooChunWook <jha970107@gmail.com>
2023-03-07 17:32:14 +0900

    feature: 랜딩페이지 생성, svg 추가

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -16,6 +16,7 @@ const ROUTES = {
   PARTY_LIST: '/party-list',
   BEST_ROUTE_LIST: '/best-route-list',
   BEST_ROUTE_DETAIL: '/best-route-detail',
+  LANDING: '/landing',
 } as const;
 
 export default ROUTES;
