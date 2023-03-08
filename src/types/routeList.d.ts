d1801ad4bced1633ff2d0e8f0a723789bb6366f3
유지영 <yj0zero@gmail.com>
2023-03-09 03:26:02 +0900

    feature: 메인 내 모임목록 api 연결

--- src/types/routeList.d.ts
+++ src/types/routeList.d.ts
@@ -6,3 +6,23 @@ export type RoutePlaceListProps = {
     image: string;
   }[];
 };
+
+export type BestRouteListType = {
+  routes: {
+    routeId: number;
+    image: string;
+    likeCount: number;
+    isLiking: boolean;
+    locations: RoutePlaceListProps.locations;
+    name: string;
+    startDate: string;
+    endDate: string;
+  }[];
+};
+
+export type BestRouteListParams = {
+  cursorId?: number;
+  pageSize: number;
+  sortType: string;
+  searchWord?: string;
+};
