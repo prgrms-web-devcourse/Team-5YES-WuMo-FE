2c4fff3fd8a8fee8df1d67f53d037180130618d7
Yuri Kim <glassk0724@gmail.com>
2023-03-10 15:53:05 +0900

    feat: 후보지 추가 확인 모달

--- src/components/party/partyPlan/PlanPlaceList.tsx
+++ src/components/party/partyPlan/PlanPlaceList.tsx
@@ -9,16 +9,19 @@ import {
   IconButton,
   Image,
   Text,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { MdAdd, MdLocationPin } from 'react-icons/md';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
+import ConfirmModal from '@/components/base/ConfirmModal';
 import { PlanPlaceListProps } from '@/types/place';
 import ROUTES from '@/utils/constants/routes';
 import { formatDateTime } from '@/utils/formatter';
 
 const PlanPlaceList = ({ places }: PlanPlaceListProps) => {
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Box padding='5'>
@@ -37,36 +40,63 @@ const PlanPlaceList = ({ places }: PlanPlaceListProps) => {
         </Button>
       </Flex>
       {places.map((place) => (
-        <Link to={`/place/${place.id}`} key={place.id}>
-          <Card
-            direction='row'
-            overflow='hidden'
-            cursor='pointer'
-            marginTop='3'
-            marginBottom='3'
-            height='7rem'>
-            <Image src={place.image} alt={place.name} width='30%' objectFit='cover' />
-            <CardBody
-              display='flex'
-              flexDirection='column'
-              justifyContent='center'
-              gap='1.5'>
-              <Heading as='h4' size='sm'>
-                {place.name}
-              </Heading>
-              <Text fontSize='sm'>{place.address}</Text>
-              <Text fontSize='xs'>{formatDateTime(place.visitDate)}</Text>
-            </CardBody>
-            <CardFooter alignItems='center' padding='0' paddingRight='3'>
-              <IconButton
-                variant='outline'
-                borderRadius='50px'
-                aria-label='일정에 후보지 추가'
-                icon={<MdAdd />}
-              />
-            </CardFooter>
-          </Card>
-        </Link>
+        <Card
+          key={place.id}
+          direction='row'
+          overflow='hidden'
+          cursor='pointer'
+          marginTop='3'
+          marginBottom='3'
+          height='7rem'>
+          <Image
+            src={place.image}
+            alt={place.name}
+            width='30%'
+            objectFit='cover'
+            onClick={() => navigate(`/place/${place.id}`)}
+          />
+          <CardBody
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            gap='1.5'
+            onClick={() => navigate(`/place/${place.id}`)}>
+            <Heading as='h4' size='sm'>
+              {place.name}
+            </Heading>
+            <Text fontSize='sm'>{place.address}</Text>
+            <Text fontSize='xs'>{formatDateTime(place.visitDate)}</Text>
+          </CardBody>
+          <CardFooter alignItems='center' padding='0' paddingRight='3'>
+            <IconButton
+              variant='outline'
+              borderRadius='50px'
+              aria-label='일정에 후보지 추가'
+              icon={<MdAdd />}
+              onClick={onOpen}
+            />
+          </CardFooter>
+          <ConfirmModal
+            hasCloseButton
+            isOpen={isOpen}
+            closeModalHandler={onClose}
+            body={
+              <Box textAlign='center'>
+                <Text>장소: {place.name}</Text>
+                <Text>방문일: {formatDateTime(place.visitDate)}</Text>
+                <Text>이 후보지를 일정을 추가할까요?</Text>
+              </Box>
+            }
+            buttonText={{ secondary: '수정', primary: '추가' }}
+            clickButtonHandler={{
+              primary: () => {
+                alert('추가 완료');
+                // TODO: 후보지 목록 업데이트
+              },
+              secondary: () => console.log('수정 페이지로 이동'), // TODO: 해당 후보지 수정 페이지로 이동
+            }}
+          />
+        </Card>
       ))}
     </Box>
   );
