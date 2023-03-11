cfff02bb748ea05e3a041e6699e3d11a1b2ed4df
유지영 <yj0zero@gmail.com>
2023-03-11 20:54:15 +0900

    feature: toast 컴포넌트구현현

---
+++ src/types/toast.d.ts
@@ -0,0 +1,26 @@
+export type CreateToastParams = {
+  message: string;
+  duration?: number;
+  type?: 'success' | 'error' | 'warning' | 'info';
+  backgroundColor?: string;
+  icon?: JSX.Element;
+  iconColor?: string;
+  fontColor?: string;
+  titleColor?: string;
+  title?: string;
+};
+
+export type CreateToastFn = (arg0: CreateToastParams) => void;
+
+export type ToastManagerProps = {
+  bind: (arg: CreateToastFn) => void;
+};
+
+export type ToastCreateType = {
+  id: number;
+} & CreateToastParams;
+
+export type ToastItemType = {
+  id?: number;
+  onDone: () => void;
+} & CreateToastParams;
