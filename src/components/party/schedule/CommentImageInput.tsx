2a0066bd9de45873975c4ecaf44e9be67761ead2
유지영 <yj0zero@gmail.com>
2023-03-12 22:00:33 +0900

    refactor: 이미지 컴포넌트 수정내용도 받을 수 있도록 수정

--- src/components/party/schedule/CommentImageInput.tsx
+++ src/components/party/schedule/CommentImageInput.tsx
@@ -1,18 +1,31 @@
 import { Button, Flex, Img, Input } from '@chakra-ui/react';
-import { ChangeEvent, InputHTMLAttributes, useRef, useState } from 'react';
+import {
+  ChangeEvent,
+  Dispatch,
+  InputHTMLAttributes,
+  SetStateAction,
+  useRef,
+  useState,
+} from 'react';
 import { Control, FieldPath, FieldValues, useController } from 'react-hook-form';
 
 interface ImageProps<T extends FieldValues>
   extends InputHTMLAttributes<HTMLInputElement> {
   control: Control<T>;
   name: FieldPath<T>;
+  setCurrentImageUrl?: Dispatch<SetStateAction<string | undefined>>;
+  imageURL?: string;
 }
 
-const CommentImageInput = <T extends FieldValues>({ name, control }: ImageProps<T>) => {
+const CommentImageInput = <T extends FieldValues>({
+  name,
+  control,
+  imageURL,
+  setCurrentImageUrl,
+}: ImageProps<T>) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const [fileName, setFileName] = useState('');
   const [value, setValue] = useState('');
-  const [imageBase64, setImageBase64] = useState<string>('');
+  const [imageBase64, setImageBase64] = useState<string>(imageURL || '');
   const { field } = useController({
     name,
     control,
@@ -34,7 +47,6 @@ const CommentImageInput = <T extends FieldValues>({ name, control }: ImageProps<
   const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      setFileName(file.name);
       encodeFileToBase64(file);
     }
     e.target.value = '';
@@ -42,7 +54,7 @@ const CommentImageInput = <T extends FieldValues>({ name, control }: ImageProps<
 
   const onFileDelete = () => {
     setImageBase64('');
-    setFileName('');
+    setCurrentImageUrl && setCurrentImageUrl('');
     field.onChange(null);
   };
 
@@ -52,45 +64,40 @@ const CommentImageInput = <T extends FieldValues>({ name, control }: ImageProps<
         w='100%'
         align='center'
         justify='space-between'
-        p='10px'
+        pt='18px'
         onClick={() => inputRef.current?.click()}>
-        <Input
-          placeholder='사진을 추가하세요'
-          defaultValue={fileName}
-          flex='1'
-          mr='1.25rem'
-          border='none'
-          borderBottom='1px solid gray'
-          borderRadius='none'
-        />
-        <Button size='sm'>사진 추가+</Button>
-        <Input
-          type='file'
-          hidden
-          {...field}
-          value={value}
-          ref={inputRef}
-          onChange={(e) => {
-            field.onChange(e.target.files?.[0]);
-            onFileChange(e);
-            setValue(e.target.value);
-          }}
-        />
+        <Button size='md' mr='auto'>
+          {!imageBase64 ? `사진 추가+` : `사진 수정`}
+        </Button>
       </Flex>
+      <Input
+        type='file'
+        hidden
+        {...field}
+        value={value}
+        ref={inputRef}
+        onChange={(e) => {
+          field.onChange(e.target.files?.[0]);
+          onFileChange(e);
+          setValue(e.target.value);
+        }}
+      />
       {imageBase64 && (
-        <Flex pos='relative' mt='0.625rem' justify='center'>
-          <Button
-            onClick={onFileDelete}
-            size='sm'
-            colorScheme='blackAlpha'
-            pos='absolute'
-            borderRadius='50%'
-            top='-4'
-            right='-4'>
-            x
-          </Button>
-          <Img w='7.5rem' h='7.5rem' src={imageBase64} />
-        </Flex>
+        <>
+          <Flex pos='relative' mt='-39px' justify='center'>
+            <Button
+              onClick={onFileDelete}
+              size='sm'
+              colorScheme='blackAlpha'
+              pos='absolute'
+              borderRadius='50%'
+              top='-4'
+              right='-4'>
+              x
+            </Button>
+            <Img w='7.5rem' h='7.5rem' src={imageBase64} />
+          </Flex>
+        </>
       )}
     </Flex>
   );
