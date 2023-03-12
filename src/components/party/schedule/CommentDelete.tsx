773b9418c7b2a7588451f2206c207b2ebc7661be
유지영 <yj0zero@gmail.com>
2023-03-12 22:02:00 +0900

    feature: 댓글 삭제 기능 구현

---
+++ src/components/party/schedule/CommentDelete.tsx
@@ -0,0 +1,53 @@
+import { Text } from '@chakra-ui/react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React from 'react';
+
+import { deleteImage } from '@/api/image';
+import { deleteRouteComment } from '@/api/schedules';
+import ConfirmModal from '@/components/base/ConfirmModal';
+
+type CommentDeleteType = {
+  isOpen: boolean;
+  onClose: () => void;
+  id: number;
+  image: string | undefined;
+};
+
+const CommentDelete = ({ isOpen, onClose, id, image }: CommentDeleteType) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteComment } = useMutation(deleteRouteComment, {
+    onSuccess: () => {
+      onClose();
+      return queryClient.invalidateQueries(['commentList']);
+    },
+  });
+
+  const { mutate: deleteCommentImage } = useMutation(deleteImage);
+  return (
+    <>
+      <ConfirmModal
+        isOpen={isOpen}
+        closeModalHandler={onClose}
+        body={
+          <Text fontSize='xl' textAlign='center'>
+            댓글을 삭제하시겠습니까?
+          </Text>
+        }
+        clickButtonHandler={{
+          primary: () => {
+            image && deleteCommentImage(image);
+            deleteComment(id);
+          },
+          secondary: () => onClose(),
+        }}
+        buttonText={{
+          primary: '삭제',
+          secondary: '취소',
+        }}
+      />
+    </>
+  );
+};
+
+export default CommentDelete;
