2bf2f6b8885dbbc8778a8d998901e1cc01334b80
MINJAE KIM <82329983+mxx-kor@users.noreply.github.com>
2023-03-24 14:26:18 +0900

    비밀번호 @ 버그 수정 (#214)
    * bug: 비밀번호 유효성 @ 금지
    
    * refactor: toOtherSign 컴포넌트 생성, hover 이벤트 추가

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -6,14 +6,15 @@ const regex_spacing = /^[^\s]*$/;
 const regex_inword = /^[^ㄱ-ㅎ|ㅏ-ㅣ]*$/;
 const regex_password = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/;
 const regex_nickname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
-const regex_nospecial = /^[a-zA-Z0-9@.]*$/;
+const regex_email_nospecial = /^[a-zA-Z0-9@.]*$/;
+const regex_nospecial = /^[a-zA-Z0-9]*$/;
 
 export const signUpSchema = yup.object({
   email: yup
     .string()
     .required(FORM_ERROR_MESSAGES.EMAIL_REQUIRED)
     .matches(regex_spacing, FORM_ERROR_MESSAGES.SPACING)
-    .matches(regex_nospecial, FORM_ERROR_MESSAGES.SPEACIAL)
+    .matches(regex_email_nospecial, FORM_ERROR_MESSAGES.SPEACIAL)
     .email(FORM_ERROR_MESSAGES.EMAIL_PATTERN),
   nickname: yup
     .string()
