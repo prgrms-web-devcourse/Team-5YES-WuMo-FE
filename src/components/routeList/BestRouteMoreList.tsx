0e0c627b43981b448bf16f7e087d551fa6cf39b6
JooChunWook <jha970107@gmail.com>
2023-04-11 14:36:44 +0900

    fix: vercel 에러 수정

--- src/components/routeList/BestRouteMoreList.tsx
+++ src/components/routeList/BestRouteMoreList.tsx
@@ -51,68 +51,72 @@ const BestRouteMoreList = () => {
     );
   if (bestRouteListError) return <></>;
 
-  return routeList.pages.map(({ routes }, index) => (
-    <React.Fragment key={index}>
-      {routes.map(
-        ({
-          routeId,
-          partyId,
-          locations,
-          image,
-          name,
-          startDate,
-          endDate,
-          likeCount,
-          isLiking,
-        }) => {
-          return (
-            <Box
-              key={routeId}
-              maxW='sm'
-              borderWidth='0.0625rem'
-              borderRadius='lg'
-              overflow='hidden'
-              border='0.0625rem solid #cfcfcf'
-              cursor='pointer'
-              margin='0 auto 2rem auto'
-              onClick={() => {
-                navigate(`/best-route/${partyId}`);
-              }}>
-              <Image
-                w='sm'
-                h='3xs'
-                objectFit='cover'
-                fallbackSrc='/skeleton.svg'
-                src={image ? image : '/logo.svg'}
-                alt={image}
-              />
-              <Box p='6'>
-                <PlaceLocationList locations={locations} />
-                <Box mb='2' fontWeight='semibold' as='h3' lineHeight='tight'>
-                  {name}
-                </Box>
-                <Text fontSize='xs'>{`${dayjs(startDate).format(
-                  'YYYY년 MM월 DD일'
-                )} ~ ${dayjs(endDate).format('YYYY년 MM월 DD일')}`}</Text>
-                <Box display='flex' justifyContent='right' mt='2' alignItems='center'>
-                  <Icon
-                    as={isLiking ? MdFavorite : MdFavoriteBorder}
-                    color='primary.red'
-                    boxSize={4}
-                    cursor='pointer'
+  return (
+    <>
+      {routeList.pages.map(({ routes }, index) => (
+        <React.Fragment key={index}>
+          {routes.map(
+            ({
+              routeId,
+              partyId,
+              locations,
+              image,
+              name,
+              startDate,
+              endDate,
+              likeCount,
+              isLiking,
+            }) => {
+              return (
+                <Box
+                  key={routeId}
+                  maxW='sm'
+                  borderWidth='0.0625rem'
+                  borderRadius='lg'
+                  overflow='hidden'
+                  border='0.0625rem solid #cfcfcf'
+                  cursor='pointer'
+                  margin='0 auto 2rem auto'
+                  onClick={() => {
+                    navigate(`/best-route/${partyId}`);
+                  }}>
+                  <Image
+                    w='sm'
+                    h='3xs'
+                    objectFit='cover'
+                    fallbackSrc='/skeleton.svg'
+                    src={image ? image : '/logo.svg'}
+                    alt={image}
                   />
-                  <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-                    {likeCount}
+                  <Box p='6'>
+                    <PlaceLocationList locations={locations} />
+                    <Box mb='2' fontWeight='semibold' as='h3' lineHeight='tight'>
+                      {name}
+                    </Box>
+                    <Text fontSize='xs'>{`${dayjs(startDate).format(
+                      'YYYY년 MM월 DD일'
+                    )} ~ ${dayjs(endDate).format('YYYY년 MM월 DD일')}`}</Text>
+                    <Box display='flex' justifyContent='right' mt='2' alignItems='center'>
+                      <Icon
+                        as={isLiking ? MdFavorite : MdFavoriteBorder}
+                        color='primary.red'
+                        boxSize={4}
+                        cursor='pointer'
+                      />
+                      <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                        {likeCount}
+                      </Box>
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
-            </Box>
-          );
-        }
-      )}
-      <div ref={setTarget}></div>
-    </React.Fragment>
-  ));
+              );
+            }
+          )}
+          <div ref={setTarget}></div>
+        </React.Fragment>
+      ))}
+    </>
+  );
 };
 
 export default BestRouteMoreList;
